e80d8e56ea840ab185282c6d2129525ecfee4a6a
Maya <mayajwong8@gmail.com>
2023-09-24 02:59:03 -0400

    trying to fix things

--- shieldher/lib/phonecall.dart
+++ shieldher/lib/phonecall.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 
-class PhoneCall extends StatelessWidget {
-  @override
+class PhoneCall extends StatelessWidget { 
+  @override 
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold (
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 82, 72, 60),
       ),
-      body: Center(child: PhoneScreen()),
-    );
+      body: Center ( 
+        child: PhoneScreen ()
+      ),
+    ); 
   }
 }
 
@@ -16,7 +18,7 @@ class PhoneScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Stack(
-      children: [
+      children: [ 
         Container(
           decoration: BoxDecoration(
             image: DecorationImage(
@@ -25,39 +27,38 @@ class PhoneScreen extends StatelessWidget {
             ),
           ),
         ),
-        Positioned(
-          bottom: 10,
-          right: 16,
-          child: ElevatedButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: Text("Location Sent"),
-                      content: Text(
-                          "Your location has been sent to your emergency contacts"),
-                      actions: [
-                        TextButton(
-                          child: Text("Close",
-                              style: TextStyle(
-                                color: Colors.black,
-                              )),
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                        ),
-                      ],
-                    );
-                  },
-                );
-              },
-              child: Text("Send Location"),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(
-                    Colors.red.withOpacity(0.7)),
-                foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
-              )),
+        Positioned (
+          bottom: 10, 
+          right: 16, 
+          child: ElevatedButton ( 
+            onPressed: () { 
+              showDialog(
+                context: context,
+                builder: (BuildContext context) { 
+                  return AlertDialog(
+                    title: Text("Location Sent"),
+                    content: Text("Your location has been sent to your emergency contacts"),
+                    actions: [
+                      TextButton(
+                        child: Text("Close", 
+                          style: TextStyle (
+                              color: Colors.black, 
+                            )),
+                        onPressed: () { 
+                          Navigator.of(context).pop(); 
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+            child: Text("Send Location"),
+            style: ButtonStyle ( 
+              backgroundColor: MaterialStateProperty.all<Color>(Colors.red.withOpacity(0.7)),
+              foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+            )
+          ),
         ),
       ],
     );
